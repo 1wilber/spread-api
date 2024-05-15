3856bcce129b2426d5952495bf9b5e3c4a1977fd
1wilber <wilber@hey.com>
2024-05-15 14:41:46 -0400

    test: add test for SpreadAlert model

--- spec/models/spread_alert_spec.rb
+++ spec/models/spread_alert_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SpreadAlert, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(market_id: 'BTC-CLP', spread: 0.05) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:market_id) }
+    it { is_expected.to validate_uniqueness_of(:market_id) }
+    it { is_expected.to validate_presence_of(:spread) }
+  end
+
+  describe '#message' do
+    context 'when current_spread is greater than spread' do
+      it 'returns :greater' do
+        expect(subject.message(0.06)).to eq(:greater)
+      end
+    end
+
+    context 'when current_spread is lower than spread' do
+      it 'returns :lower' do
+        expect(subject.message(0.04)).to eq(:lower)
+      end
+    end
+
+    context 'when current_spread is equal to spread' do
+      it 'returns :equal' do
+        expect(subject.message(0.05)).to eq(:equal)
+      end
+    end
+  end
 end
